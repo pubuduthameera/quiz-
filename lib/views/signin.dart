9fa536cf2fe6b89bdeeb73f523dee0ddb71a9fbe
MUHAMAD KHURAM <muhamadkhuram207@gmail.com>
2020-10-18 16:42:59 +0500

    init

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -29,7 +29,7 @@ class _SignInState extends State<SignIn> {
           });
           HelperFunctions.saveUserLoggedInDetails(isLoggedin: true);
           Navigator.pushReplacement(
-              context, MaterialPageRoute(builder: (context) => Home()));
+              context, MaterialPageRoute(builder: (context) => HomePage()));
         }
       });
     }
@@ -46,83 +46,80 @@ class _SignInState extends State<SignIn> {
       ),
       body: _isLoading
           ? Container(
-        child: Center(
-          child: CircularProgressIndicator(),
-        ),
-      )
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
           : Form(
-        key: _formkey,
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            children: [
-              Spacer(),
-              TextFormField(
-                validator: (val) {
-                  return val.isEmpty ? "Enter Email id" : null;
-                },
-                decoration: InputDecoration(hintText: "Email"),
-                onChanged: (val) {
-                  email = val;
-                },
-              ),
-              SizedBox(
-                height: 6,
-              ),
-              TextFormField(
-                obscureText: true,
-                validator: (val) {
-                  return val.isEmpty ? "Enter Password" : null;
-                },
-                decoration: InputDecoration(hintText: "Password"),
-                onChanged: (val) {
-                  password = val;
-                },
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              GestureDetector(
-                onTap: () {
-                  signIn();
-                },
-                child: blueButton(
-                    context: context,
-                    label: "Sign In"
+              key: _formkey,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  children: [
+                    Spacer(),
+                    TextFormField(
+                      validator: (val) {
+                        return val.isEmpty ? "Enter Email id" : null;
+                      },
+                      decoration: InputDecoration(hintText: "Email"),
+                      onChanged: (val) {
+                        email = val;
+                      },
+                    ),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      validator: (val) {
+                        return val.isEmpty ? "Enter Password" : null;
+                      },
+                      decoration: InputDecoration(hintText: "Password"),
+                      onChanged: (val) {
+                        password = val;
+                      },
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        signIn();
+                      },
+                      child: blueButton(context: context, label: "Sign In"),
+                    ),
+                    SizedBox(
+                      height: 18,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don't have an account?",
+                          style: TextStyle(fontSize: 15.5),
+                        ),
+                        GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SignUp()));
+                            },
+                            child: Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                  fontSize: 15.5,
+                                  decoration: TextDecoration.underline),
+                            ))
+                      ],
+                    ),
+                    SizedBox(
+                      height: 80,
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(
-                height: 18,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account?",
-                    style: TextStyle(fontSize: 15.5),
-                  ),
-                  GestureDetector(
-                      onTap: () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SignUp()));
-                      },
-                      child: Text(
-                        "Sign Up",
-                        style: TextStyle(
-                            fontSize: 15.5,
-                            decoration: TextDecoration.underline),
-                      ))
-                ],
-              ),
-              SizedBox(
-                height: 80,
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
